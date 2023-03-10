40adf96ff2a3a77ef9aa14f940236d2b11c6e308
Nguyentraipc <127364998+Nguyentraipc@users.noreply.github.com>
2023-03-10 10:07:20 +0700

    Delete rpc_mining.sh

--- rpc_mining.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
-while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6du79z5kvhtl6s5095v4 -r comunity-pools.mysrv.cloud:10300 -r1 dero.friendspool.club:10300 -p rpc;
-    sleep 5;
-done
